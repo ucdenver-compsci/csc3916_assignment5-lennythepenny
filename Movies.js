3fa3c5db488aefab5817acd4a5ece6564f297526
lennythepenny <lena.sarieva@gmail.com>
2024-04-10 21:39:08 -0600

    mongo not working

--- Movies.js
+++ Movies.js
@@ -1,23 +1,51 @@
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DB);
+// var mongoose = require('mongoose');
+// mongoose.connect(process.env.DB);
 
-const MovieSchema = new mongoose.Schema({
-  title: { type: String, required: true, index: true },
-    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
-    genre: {
-      type: String,
-      enum: [
-        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-      ],
-    },
-    actors: [{
-      actorName: String, 
-      characterName: String,
-    }],
-    imageUrl: { type: String }, 
-  }, { collection : 'movies' });
+// const MovieSchema = new mongoose.Schema({
+//   title: { type: String, required: true, index: true },
+//     releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+//     genre: {
+//       type: String,
+//       enum: [
+//         'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+//       ],
+//     },
+//     actors: [{
+//       actorName: String, 
+//       characterName: String,
+//     }],
+//     imageUrl: { type: String }, 
+//   }, { collection : 'movies' });
   
 
-//return the model
+// //return the model
+// const MovieModel = mongoose.model('Movie', MovieSchema);
+// module.exports = MovieModel;
+const mongoose = require('mongoose');
+
+try {
+    mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected");
+} catch (error) {
+    console.log("Could not connect to MongoDB:", error);
+}
+
+const MovieSchema = new mongoose.Schema({
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+    genre: {
+        type: String,
+        enum: [
+            'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+        ],
+    },
+    actors: [{
+        actorName: String, 
+        characterName: String,
+    }],
+    imageUrl: { type: String }, 
+}, { collection: 'movies' });
+
+// Return the model
 const MovieModel = mongoose.model('Movie', MovieSchema);
 module.exports = MovieModel;
