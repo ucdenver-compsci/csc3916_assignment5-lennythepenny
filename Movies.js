b467f7b5662be032ed903ec491ba3cf8c644d741
lennythepenny <lena.sarieva@gmail.com>
2024-04-10 22:04:35 -0600

    mongo still not working

--- Movies.js
+++ Movies.js
@@ -1,51 +1,23 @@
-// var mongoose = require('mongoose');
-// mongoose.connect(process.env.DB);
-
-// const MovieSchema = new mongoose.Schema({
-//   title: { type: String, required: true, index: true },
-//     releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
-//     genre: {
-//       type: String,
-//       enum: [
-//         'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-//       ],
-//     },
-//     actors: [{
-//       actorName: String, 
-//       characterName: String,
-//     }],
-//     imageUrl: { type: String }, 
-//   }, { collection : 'movies' });
-  
-
-// //return the model
-// const MovieModel = mongoose.model('Movie', MovieSchema);
-// module.exports = MovieModel;
-const mongoose = require('mongoose');
-
-try {
-    mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("MongoDB connected");
-} catch (error) {
-    console.log("Could not connect to MongoDB:", error);
-}
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB);
 
 const MovieSchema = new mongoose.Schema({
-    title: { type: String, required: true, index: true },
+  title: { type: String, required: true, index: true },
     releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
     genre: {
-        type: String,
-        enum: [
-            'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-        ],
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
     },
     actors: [{
-        actorName: String, 
-        characterName: String,
+      actorName: String, 
+      characterName: String,
     }],
     imageUrl: { type: String }, 
-}, { collection: 'movies' });
+  }, { collection : 'movies' });
+  
 
-// Return the model
+//return the model
 const MovieModel = mongoose.model('Movie', MovieSchema);
 module.exports = MovieModel;
