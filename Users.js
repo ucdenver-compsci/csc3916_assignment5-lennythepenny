e61a1c6099e986f000061d533c494da15a28a00c
lennythepenny <lena.sarieva@gmail.com>
2024-04-18 13:34:31 -0600

    rewrite the data

--- Users.js
+++ Users.js
@@ -45,47 +45,4 @@ UserSchema.methods.comparePassword = function (password, callback) {
 
 //return the model to server
 module.exports = mongoose.model('User', UserSchema);
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-// var bcrypt = require('bcrypt-nodejs');
 
-// mongoose.Promise = global.Promise;
-
-// try {
-//     mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log("MongoDB connected");
-// } catch (error) {
-//     console.log("Could not connect to MongoDB:", error);
-// }
-
-// var UserSchema = new Schema({
-//     name: String,
-//     username: { type: String, required: true, index: { unique: true }},
-//     password: { type: String, required: true, select: false }
-// });
-
-// UserSchema.pre('save', function(next) {
-//     var user = this;
-
-//     if (!user.isModified('password')) return next();
-
-//     bcrypt.hash(user.password, null, null, function(err, hash) {
-//         if (err) return next(err);
-
-//         user.password = hash;
-//         next();
-//     });
-// });
-
-// UserSchema.methods.comparePassword = function (password) {
-//     var user = this;
-
-//     return new Promise((resolve, reject) => {
-//         bcrypt.compare(password, user.password, function(err, isMatch) {
-//             if (err) return reject(err);
-//             resolve(isMatch);
-//         });
-//     });
-// };
-
-// module.exports = mongoose.model('User', UserSchema);
